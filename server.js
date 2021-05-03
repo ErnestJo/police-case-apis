ded7d664021a8ba746e3e084017785f464a43e60
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-03 21:02:55 +0300

    morgan logger was added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
 
 //Route files
 const cases = require('./routes/cases');
@@ -8,6 +9,11 @@ const cases = require('./routes/cases');
 dotenv.config({  path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 //Mount router
 app.use('/api/v1/cases', cases);
