97f320d71252d9e05a561202aa784d9e01044f7e
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-03 11:05:16 +0300

    put, delete, update routes

--- routes/cases.js
+++ routes/cases.js
@@ -4,3 +4,21 @@ const router = express.Router();
 router.get('/api/v1/cases', (req, res) => {
     res.status(200).json({ success: true, msg: 'show all cases' });
 });
+
+
+router.get('/api/v1/cases/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `show cases ${req.params.id}`});
+});
+
+
+router.post('/api/v1/cases', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create a case' });
+});
+
+router.put('/api/v1/cases/:id', (req, res) => {
+    res.status(200).json({ success: true, msg:`Update case ${req.params.id}` });
+});
+
+router.get('/api/v1/cases/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete case ${req.params.id}` });
+});
