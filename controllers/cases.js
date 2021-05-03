673cf2852b070a87f8abc532086b42ea1844d04d
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-03 20:34:29 +0300

    case router configured

--- controllers/cases.js
+++ controllers/cases.js
@@ -11,13 +11,13 @@ exports.getCases = (req, res, next) => {
 // @route     Get /api/v1/cases/:id
 // @access    system Users
 exports.getCase = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `show cases ${req.params.id}`});
+    res.status(200).json({ success: true, msg: `show case ${req.params.id}`});
 }
 
 // @desc      create Cases
 // @route     Post /api/v1/cases
 // @access    system Users
-exports.postCase = (req, res, next) => {
+exports.createCase = (req, res, next) => {
     res.status(200).json({ success: true, msg: 'Create a case' });
 }
 
@@ -26,13 +26,13 @@ exports.postCase = (req, res, next) => {
 // @desc      Update case
 // @route     Update or put /api/v1/cases/:id
 // @access    system Users
-exports.putCase = (req, res, next) => {
+exports.updateCase = (req, res, next) => {
     res.status(200).json({ success: true, msg:`Update case ${req.params.id}` });
 }
 
 // @desc      delete case
 // @route     delete case/api/v1/cases/:id
 // @access    system Users
-exports.putCase = (req, res, next) => {
+exports.deleteCase = (req, res, next) => {
     res.status(200).json({ success: true, msg: `Delete case ${req.params.id}` });
 }
--- routes/cases.js
+++ routes/cases.js
@@ -1,38 +1,20 @@
 const express = require('express');
+const { getCases,
+    getCase,
+    createCase,
+    updateCase,
+    deleteCase } = require('../controllers/cases');
+
 const router = express.Router();
 
-
-
-
-
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'show all cases' });
-});
-
-
-
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `show cases ${req.params.id}`});
-});
-
-
-
-
-router.post('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create a case' });
-});
-
-
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg:`Update case ${req.params.id}` });
-});
-
-
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete case ${req.params.id}` });
-});
+router.route('/')
+     .get(getCases)
+     .post(createCase);
+     
+router
+    .route('/:id')
+    .get(getCase)
+    .put(updateCase)
+    .delete(deleteCase)
 
 module.exports = router;
