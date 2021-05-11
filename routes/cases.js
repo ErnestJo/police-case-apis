8066fd28f4f468ad0f079039a25973e1bdc4591e
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-11 04:10:51 +0300

    added reroute configations in Case route file

--- routes/cases.js
+++ routes/cases.js
@@ -11,11 +11,13 @@ const advancedResults = require('../middleware/advancedResults');
 
 //include other resource routers
 const accuserRouter = require('./accusers');
+const investigationReportRouter = require('./investigationReports');
 
 const router = express.Router();
 
 // Reroute into other resource router
 router.use('/:caseId/accusers', accuserRouter);
+router.use('/:caseId/investigationReports', investigationReportRouter);
 
 router.route('/')
      .get(advancedResults(Case, 'accusers'),getCases)
