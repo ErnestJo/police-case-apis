65e7de1a2cadc7affad41b5b8c13f02c564315a5
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-13 11:23:12 +0300

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 
 
 // @desc      Register User
-// @route     Get /api/v1/auth/register
+// @route     POST /api/v1/auth/register
 // @access    system User
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -26,4 +26,36 @@ exports.register = asyncHandler(async (req, res, next) => {
  res.status(200).json({ success: true, token})
 
 
+});
+
+
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const token = user.getSignedJwtToken();
+  
+  res.status(200).json({ success: true, token})
 });
