2c17cdf195045157b128e07ceabbc61b8f2e4ace
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-16 10:31:51 +0300

    Get me details fix route

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ const { register,login , getMe} = require('../controllers/auth');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router
