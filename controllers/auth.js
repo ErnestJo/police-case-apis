b8b0b4c5feda97b978694d345ef07818f9882fa5
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-17 00:50:59 +0300

    fixing bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
