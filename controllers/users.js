52f8327bb66957ec2fa925cfb45c3ad85d4b7499
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-18 15:29:52 +0300

    get asingle  users

--- controllers/users.js
+++ controllers/users.js
@@ -7,4 +7,16 @@ const User = require('../models/User');
 // @access    Private/Admin
 exports.getUsers = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
+});
+  
+// @desc      Get single user
+// @route     GET /api/v1/auth/users/:id
+// @access    Private/Admin
+exports.getUser = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+  
+    res.status(200).json({
+      success: true,
+      data: user
+    });
   });
