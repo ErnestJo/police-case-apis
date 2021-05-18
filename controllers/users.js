c2a690a51a0459383bdd0dc091b5a796f5423eb8
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-18 15:30:32 +0300

    create a  user

--- controllers/users.js
+++ controllers/users.js
@@ -19,4 +19,16 @@ exports.getUser = asyncHandler(async (req, res, next) => {
       success: true,
       data: user
     });
+});
+  
+// @desc      Create user
+// @route     POST /api/v1/auth/users
+// @access    Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body);
+  
+    res.status(201).json({
+      success: true,
+      data: user
+    });
   });
