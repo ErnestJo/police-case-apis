4c9c5f0773a4e7f25d02e5cbcc0b37e37639ecb5
Ernest Joseph <smartlabstech@yahoo.com>
2021-05-18 14:53:45 +0300

    update  password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register,login , getMe, forgotPassword, resetPassword} = require('../controllers/auth');
+const { register,login , getMe, forgotPassword, resetPassword, updateDetails } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updateDetails', protect, updateDetails)
 router.post('/forgotPassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
